48fac263f8d6dbbe4bb1a7a1391bbb671166680b
izaid <hi@irwinzaid.com>
2017-08-02 14:45:44 +0100

    Updated formula

--- app.rb
+++ app.rb
@@ -1,24 +1,17 @@
-bottle do
-  root_url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-  prefix "/opt/homebrew"
-  cellar "/opt/homebrew/Cellar"
-  rebuild 4
+class App < Formula
+  desc "dingo"
+  homepage "https://github.com/see-through"
+  url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
   sha256 "186e749d5adf66b11d199310178efed3c8f33ad7cf243cb55766d93f73f44ccf"
-end
 
-#class App < Formula
-#  desc "dingo"
-#  homepage "https://github.com/see-through"
-#  url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-#  sha256 "186e749d5adf66b11d199310178efed3c8f33ad7cf243cb55766d93f73f44ccf"
 #
-#  bottle :unneeded
-#
-#  def install
-#    bin.install "dingo"
-#  end
+  bottle :unneeded
+
+  def install
+    (lib/'libdingo_app.dylib').install "lib/libdingo_app.dylib"
+  end
 
 #  test do
 #    system "#{bin}/app", "--version"
 #  end
-#end
+end
