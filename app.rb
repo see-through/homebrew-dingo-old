3dc1d4f61dc7d778af15880917644efb9630ea44
izaid <hi@irwinzaid.com>
2017-08-02 16:46:05 +0100

    Update

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Formula
   desc "dingo"
   homepage "https://github.com/see-through"
   url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-  sha256 "186e749d5adf66b11d199310178efed3c8f33ad7cf243cb55766d93f73f44ccf"
+  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
 
 #
   bottle :unneeded
