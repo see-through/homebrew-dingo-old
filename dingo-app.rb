3775e5372376b6f7ab8fe5bdca448da71961e8b1
izaid <hi@irwinzaid.com>
2017-08-02 20:38:31 +0100

    Added dingo-app file

---
+++ dingo-app.rb
@@ -0,0 +1,16 @@
+class DingoApp < Formula
+  desc "dingo"
+  homepage "https://github.com/see-through"
+  url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
+  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
+
+  bottle :unneeded
+
+  def install
+    lib.install "lib/libdingo_app.dylib"
+  end
+
+#  test do
+#    system "#{bin}/app", "--version"
+#  end
+end
