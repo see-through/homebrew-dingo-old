8013fd863c9b2fb1a06781e580bc68d9fce1a319
izaid <hi@irwinzaid.com>
2017-08-02 21:32:28 +0100

    Removed sha256

--- dingo-app.rb
+++ dingo-app.rb
@@ -2,7 +2,7 @@ class DingoApp < Formula
   desc "dingo"
   homepage "https://github.com/see-through"
   url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
+#  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
 
   bottle :unneeded
 
