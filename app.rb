c0d8ea4b53ed4fb3c841404abe0330fbbe61d50b
izaid <hi@irwinzaid.com>
2017-08-02 16:48:56 +0100

    Update

--- app.rb
+++ app.rb
@@ -4,19 +4,10 @@ class App < Formula
   url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
   sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
 
-#
   bottle :unneeded
 
   def install
-    p `pwd`
-    p `ls`
-
     lib.install "lib/libdingo_app.dylib"
-
-#    puts "Hello, world!"
-#    puts "Hello, world! 2"
-#    puts Dir.pwd
-#    (lib/'libdingo_app.dylib').install "lib/libdingo_app.dylib"
   end
 
 #  test do
