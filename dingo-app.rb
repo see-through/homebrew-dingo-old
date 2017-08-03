b954f1a8271b04739a56b581c2ccf44de4ef9668
izaid <hi@irwinzaid.com>
2017-08-03 08:27:30 +0100

    Added devel to formula

--- dingo-app.rb
+++ dingo-app.rb
@@ -2,9 +2,13 @@ class DingoApp < Formula
   desc "dingo"
   homepage "https://github.com/see-through"
   url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-#  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
+  sha256 "6689c9f47935d291aa479bd76da26c6942b2808293c422fbff04eb5199891383"
 
   bottle :unneeded
+
+  devel do
+    url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1-cc2fbd4-darwin-app.tar.gz"
+  end
 
   def install
     lib.install "lib/libdingo_app.dylib"
