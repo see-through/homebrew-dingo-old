a5880f8d4785829dcaf2d3f81e6d416a0ad49576
izaid <hi@irwinzaid.com>
2017-08-04 10:51:48 +0100

    Update

--- dingo-app.rb
+++ dingo-app.rb
@@ -2,7 +2,9 @@ class DingoApp < Formula
   desc "dingo"
   homepage "https://github.com/see-through"
   url "https://s3-eu-west-1.amazonaws.com/see-through-dingo/dingo-0.1.1-darwin-app.tar.gz"
-  sha256 File.read(File.join(File.dirname(__FILE__), 'test')).strip
+
+  sha256_filename File.join(File.dirname(__FILE__), 'dingo-app-sha256')
+  sha256 File.read(sha256_filename).strip
 
   bottle :unneeded
 
@@ -11,8 +13,6 @@ class DingoApp < Formula
   end
 
   def install
-    puts File.read(File.join(File.dirname(__FILE__), 'test'))
-
     lib.install "lib/libdingo_app.dylib"
   end
 
